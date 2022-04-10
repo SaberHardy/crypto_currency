93d4680a3446893b2b3ff4b5754dd3d03d31a979
SaberHardy <samirsamado5@gmail.com>
2022-04-10 08:51:20 +0300

    signals & periodic code

--- positions/tasks.py
+++ positions/tasks.py
@@ -5,6 +5,7 @@ from celery.schedules import crontab
 from celery.task import periodic_task
 
 from .models import Test
+from .utils import get_random_code
 
 
 @shared_task
@@ -12,7 +13,14 @@ def create_test_obj(name):
     Test.objects.create(name=name)
 
 
-@periodic_task(run_every=timedelta(seconds=3))
-def ren_create_objects():
-    create_test_obj.delay(name='create_obj')
+@shared_task
+def create_signals_code():
+    tests = Test.objects.all()
+    for test in tests:
+        test.code = get_random_code()
+        test.save()
 
+
+@periodic_task(run_every=(crontab(minute='*/1')))
+def run_create_objects():
+    create_test_obj.delay(name='create_obj')
